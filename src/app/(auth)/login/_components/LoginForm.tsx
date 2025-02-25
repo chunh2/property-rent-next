a2ea0f49ef542a6574f2241ec8decb30ae557dbd
CH <chunhwa5138@outlook.com>
2025-02-25 12:27:46 +0800

    Add radio button for role

--- src/app/(auth)/login/_components/LoginForm.tsx
+++ src/app/(auth)/login/_components/LoginForm.tsx
@@ -7,6 +7,7 @@ import { Input } from "@/components/ui/input";
 import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const LoginFormSchema = z.object({
   email: z
@@ -15,6 +16,9 @@ const LoginFormSchema = z.object({
     .max(100, "Email cannot exceed 100 characters")
     .email("Invalid email format"),
   password: z.string().min(8, "At least 8 characters"),
+  roleId: z.enum(["1", "2"], {
+    errorMap: () => ({ message: "Role is required" }),
+  }),
 });
 
 type LoginFormType = z.infer<typeof LoginFormSchema>;
@@ -36,7 +40,7 @@ function LoginForm() {
     <>
       <form onSubmit={handleSubmit(handleLogin)}>
         <CardContent className="my-5">
-          <div className="my-5">
+          <div className="my-3">
             {/* Email */}
             <Label htmlFor="email">Email</Label>
 
@@ -55,7 +59,7 @@ function LoginForm() {
             </div>
           </div>
 
-          <div>
+          <div className="my-3">
             {/* Password */}
             <Label htmlFor="password">Password</Label>
 
@@ -77,6 +81,39 @@ function LoginForm() {
             <div className="mt-2 ml-2 h-6">
               <p className="text-red-500">{errors.password?.message || null}</p>
             </div>
+          </div>
+
+          <div className="my-3">
+            {/* RoleId */}
+            <Label>Login as</Label>
+
+            <Controller
+              name="roleId"
+              control={control}
+              defaultValue={undefined}
+              render={({ field: { onChange, value } }) => (
+                <RadioGroup
+                  className="flex justify-start"
+                  onValueChange={onChange}
+                  value={value}
+                >
+                  <div className="flex items-center mt-1">
+                    <RadioGroupItem value="1" id="1" className="mx-1" />
+                    <Label htmlFor="1">Tenant</Label>
+                  </div>
+
+                  <div className="flex items-center">
+                    <RadioGroupItem value="2" id="2" className="mx-1" />
+                    <Label htmlFor="2">Owner</Label>
+                  </div>
+                </RadioGroup>
+              )}
+            />
+          </div>
+
+          {/* RoleId error message */}
+          <div className="mt-2 ml-2 h-6">
+            <p className="text-red-500">{errors.roleId?.message || null}</p>
           </div>
         </CardContent>
 
