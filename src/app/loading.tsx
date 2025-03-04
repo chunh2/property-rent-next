2d97cb57270350da74835d66b1b20380eaf6be08
CH <chunhwa5138@outlook.com>
2025-03-04 15:43:08 +0800

    Added global level loading UI

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from "./_utilsComponents/Loading";
+
+function loading() {
+  return (
+    <div className="w-screen h-screen">
+      <Loading />
+    </div>
+  );
+}
+
+export default loading;
