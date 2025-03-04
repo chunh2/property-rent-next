d966b55b57f0e0411e5df271c2113c9b09dd8f43
CH <chunhwa5138@outlook.com>
2025-03-04 10:49:44 +0800

    Create type of image for preview and as props

---
+++ src/app/(owner)/owner-properties/utils/PropertyImageType.ts
@@ -0,0 +1,13 @@
+type PropertyImageType = {
+  id: string;
+  property_image: File;
+};
+
+export type PropertyImageFileType = {
+  name: string;
+  size: number;
+  lastModified: number;
+  type: string;
+};
+
+export default PropertyImageType;
