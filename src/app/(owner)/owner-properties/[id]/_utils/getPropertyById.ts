fa23c11b727e80fd1c279f43549a87a2e10e237b
CH <chunhwa5138@outlook.com>
2025-03-04 19:15:06 +0800

    Create function to fetch property by id

---
+++ src/app/(owner)/owner-properties/[id]/_utils/getPropertyById.ts
@@ -0,0 +1,27 @@
+import getToken from "@/utils/getToken";
+
+const getPropertyById = async (id: string | number) => {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const url = `${API_URL}/api/owner/properties/${id}`;
+
+  const token = getToken();
+
+  const res = await fetch(url, {
+    next: {
+      revalidate: 60,
+    },
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+    },
+  });
+
+  const response = await res.json();
+
+  console.log(response);
+
+  return response;
+};
+
+export default getPropertyById;
