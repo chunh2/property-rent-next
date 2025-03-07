4bdfee97bb4a70dfd4f76825c37b90f60ee56747
CH <chunhwa5138@outlook.com>
2025-03-07 22:08:10 +0800

    Create component for logout button with confirmation dialog.

---
+++ src/app/_utilsComponents/Logout.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { AlertDialogAction } from "@radix-ui/react-alert-dialog";
+import { useState } from "react";
+import deleteCookies from "../_utils/deleteCookies";
+import { useRouter } from "next/navigation";
+
+function Logout() {
+  const [logoutDialogOpen, setLogoutDialogOpen] = useState(false);
+
+  const router = useRouter();
+
+  const logout = () => {
+    deleteCookies("accessToken");
+
+    router.replace("/login");
+  };
+
+  return (
+    <>
+      <AlertDialog open={logoutDialogOpen} onOpenChange={setLogoutDialogOpen}>
+        <AlertDialogTrigger asChild>
+          <Button variant="destructive">Logout</Button>
+        </AlertDialogTrigger>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confirm Logout?</AlertDialogTitle>
+
+            <AlertDialogDescription>
+              Are you sure you want to log out?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+
+            <AlertDialogAction asChild>
+              <Button variant="destructive" onClick={logout}>
+                Logout
+              </Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}
+
+export default Logout;
