dab0053bf35b33d128028d7b9e9dd2d825edcdb3
CH <chunhwa5138@outlook.com>
2025-03-08 16:01:28 +0800

    Added Property Type

---
+++ src/app/(tenant)/tenant-properties/_utils/PropertyType.ts
@@ -0,0 +1,46 @@
+type PropertyType = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  address: string;
+  city: string;
+  bedroom: number;
+  bathroom: number;
+  property_type_id: number;
+  user_id: number;
+  state_id: number;
+  property_status_id: number;
+  createdAt: string;
+  updatedAt: string;
+
+  property_type: PropertyTypeType;
+
+  state: PropertyStateType;
+
+  property_status: PropertyStatusType;
+
+  property_images: PropertyImageType[];
+};
+
+type PropertyTypeType = {
+  id: number;
+  name: string;
+};
+
+type PropertyStateType = {
+  id: number;
+  name: string;
+};
+
+type PropertyStatusType = {
+  id: number;
+  name: string;
+};
+
+type PropertyImageType = {
+  id: number;
+  image_path: string;
+};
+
+export default PropertyType;
