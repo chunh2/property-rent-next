bf14eca7645d193785fb949d2e963c21a3f199ab
CH <chunhwa5138@outlook.com>
2025-03-08 16:04:47 +0800

    Added rendering data

---
+++ src/app/(tenant)/tenant-properties/_component/PropertyCard.tsx
@@ -0,0 +1,53 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import PropertyType from "../_utils/PropertyType";
+import Image from "next/image";
+import Link from "next/link";
+
+function PropertyCard({ property }: { property: PropertyType }) {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  const { id } = property;
+
+  return (
+    <>
+      <Link href={`/owner-properties/${id}`}>
+        <Card>
+          <CardHeader className="w-full p-0">
+            <div className="text-center">
+              <Image
+                src={`${API_URL}/${property.property_images[0]?.image_path}`}
+                alt={`${property.property_type.name} - ${property.title}`}
+                width={800}
+                height={600}
+                className="w-full h-full object-cover rounded-t-lg"
+              />
+            </div>
+          </CardHeader>
+
+          <CardContent className="mt-4">
+            <h6 className="text-lg">{property.title}</h6>
+            <p className="font-bold text-sm">RM {property.price}</p>
+
+            <div className="flex gap-5">
+              <div className="flex">
+                <p className="text-sm">{property.bedroom} Bedroom</p>
+              </div>
+
+              <div className="flex">
+                <p className="text-sm">{property.bathroom} Bathroom</p>
+              </div>
+            </div>
+
+            <div>
+              <p className="">
+                {property.city}, {property.state.name}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </Link>
+    </>
+  );
+}
+
+export default PropertyCard;
--- src/app/(tenant)/tenant-properties/page.tsx
+++ src/app/(tenant)/tenant-properties/page.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { redirect } from "next/navigation";
+import getProperties from "./_utils/getProperties";
+import PropertyCard from "./_component/PropertyCard";
+import PropertyType from "./_utils/PropertyType";
 
-function TenantProperties() {
-  return <div>TenantProperties</div>;
+async function TenantProperties({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[]>;
+}) {
+  // default
+  const page = "1";
+  const limit = "20";
+
+  if (!searchParams.page || !searchParams.limit) {
+    redirect(`/tenant-properties?page=${page}&limit=${limit}`);
+    return;
+  }
+
+  const query = new URLSearchParams();
+
+  // appending searchParams to URLSearchParams (different types)
+  Object.entries(searchParams).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((v) => query.append(key, v));
+    } else {
+      query.append(key, value);
+    }
+  });
+
+  const {
+    message,
+    count,
+    data: properties,
+    error,
+  }: Response = await getProperties(query);
+
+  return (
+    <>
+      <div className="mx-5 my-3 sm:mx-8 sm:my-4 md:mx-10 md:my-6 lg:mx-12 lg:my-8 xl:mx-14 xl:my-10 2xl:mx-16 2xl:my-12">
+        <h1 className="text-center font-bold text-4xl">Properties</h1>
+
+        <div className="my-5">
+          {properties?.length ?? 0 > 0 ? (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 sm:gap-3 lg:gap-4 xl:gap-5 2xl:gap-6">
+              {properties?.map((property: PropertyType) => (
+                <PropertyCard key={property.id} property={property} />
+              ))}
+            </div>
+          ) : (
+            <p className="text-gray-500 text-center text-2xl font-bold">
+              {error || "Properties not found"}
+            </p>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TenantProperties;
+
+export const metadata = {
+  title: "Properties | Tenant",
+};
+
+type Response = {
+  message?: string;
+  data?: PropertyType[];
+  count?: number;
+  error?: string;
+};
