20941906a478fcdd63f49ac8579a7d9ac1cabf3f
CH <chunhwa5138@outlook.com>
2025-03-11 20:39:40 +0800

    Removed logout button.

--- src/app/(owner)/owner-properties/layout.tsx
+++ src/app/(owner)/owner-properties/layout.tsx
@@ -1,12 +1,6 @@
-import Logout from "@/app/_utilsComponents/Logout";
-
 function LayoutOwnerProperties({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex justify-end m-5">
-        <Logout />
-      </div>
-
       <>{children}</>
     </>
   );
