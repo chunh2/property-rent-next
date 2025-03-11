7e37a1a4c44aae7140b84f389d5adf06ec2a7094
CH <chunhwa5138@outlook.com>
2025-03-11 20:25:44 +0800

    Added loading UI.

---
+++ src/app/(owner)/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from "../_utilsComponents/Loading";
+
+function loading() {
+  return (
+    <div className="flex justify-center items-center h-[90vh]">
+      <Loading />
+    </div>
+  );
+}
+
+export default loading;
