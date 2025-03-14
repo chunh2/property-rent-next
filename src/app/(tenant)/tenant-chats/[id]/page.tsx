beda33ebebc3b70e07910100e5155653d18d73fc
CH <chunhwa5138@outlook.com>
2025-03-14 23:57:01 +0800

    Added rendering messages.

---
+++ src/app/(tenant)/tenant-chats/[id]/page.tsx
@@ -0,0 +1,23 @@
+import NavigateBack from "@/app/_utilsComponents/NavigateBack";
+import MessagesSection from "./_component/MessagesSection";
+import getMessages from "./_utils/getMessages";
+
+function ChatRoom() {
+  return (
+    <>
+      <div className="mx-2 sm:mx-20">
+        <div className="fixed top-20 sm:top-28">
+          <NavigateBack buttonVariant="ghost" />
+        </div>
+
+        <MessagesSection />
+      </div>
+    </>
+  );
+}
+
+export const metadata = {
+  title: "Private Chat",
+};
+
+export default ChatRoom;
