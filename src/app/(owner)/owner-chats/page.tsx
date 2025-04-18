c48724c0bbbeeef1dc37519fc013f2a5fda21c9d
CH <chunhwa5138@outlook.com>
2025-04-18 20:18:14 +0800

    Removed unused variable.

--- src/app/(owner)/owner-chats/page.tsx
+++ src/app/(owner)/owner-chats/page.tsx
@@ -1,10 +1,6 @@
-import { cookies } from "next/headers";
 import ChatRooms from "./_component/ChatRooms";
 
 function Chats() {
-  const cookiesInstance = cookies();
-  const accessToken = cookiesInstance.get("accessToken")?.value;
-
   return (
     <>
       <h1 className="font-bold text-center text-4xl">Chats</h1>
